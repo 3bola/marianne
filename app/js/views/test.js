3e85af6947fff0211d2395338e850f3c0820414f
Juri Saltbacka <juri@3bola.io>
2014-11-14 12:30:09 +0200

    Added empty folders

--- app/js/views/test.js
+++
@@ -1,3 +0,0 @@
-
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInZpZXdzL3Rlc3QuY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUVxQyIsImZpbGUiOiJ2aWV3cy90ZXN0LmpzIiwic291cmNlUm9vdCI6Ii9zb3VyY2UvIiwic291cmNlc0NvbnRlbnQiOlsiIl19
